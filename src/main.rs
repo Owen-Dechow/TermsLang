68ebbf8bd9bfa8ac48dc8d089974883362595881
Owen Dechow <owen,dechow@gmail.com>
2024-02-20 20:17:20 -0500

    Update transpiler name

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 pub mod errors;
 pub mod lexer;
 pub mod parser;
-mod transpiler;
+mod terms2rust_transpiler;
 
 use std::fs;
 
@@ -26,7 +26,7 @@ fn main() {
     let parse_out = parser::parse(lex_out);
 
     match &parse_out {
-        Ok(block) => match transpiler::transpile(block) {
+        Ok(block) => match terms2rust_transpiler::transpile(block) {
             Ok(out) => {
                 let _ = fs::write("output/src/main.rs", out);
             }
