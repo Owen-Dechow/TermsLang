4d7a8c3df078bc2fb40d96fe7d38631c67ca8129
Owen Dechow <owen.dechow@gmail.com>
2024-10-18 12:28:02 -0400

    Remove type association

--- src/parser/parse_type.rs
+++ src/parser/parse_type.rs
@@ -7,51 +7,6 @@ use crate::{
 
 use super::{parse_object::parse_object_peekable, TokenStream, Type, VarSigniture};
 
-// Get types within <>
-pub fn get_associated_types(
-    token_stream: &mut TokenStream,
-    file: &PathBuf,
-) -> Result<Vec<Type>, ParserError> {
-    // Create the type list
-    let mut associated_types = Vec::<Type>::new();
-
-    loop {
-        // Add clause to check for empty type list: <>
-        if let Some(Token(TokenType::Operator(Operator::Greater), _)) = token_stream.advance() {
-            break;
-        } else {
-            token_stream.back();
-        }
-
-        // Parse type
-        let associated_type = parse_type(token_stream, file)?;
-
-        // Add valid type to list
-        associated_types.push(associated_type);
-
-        // Check for , else if > break loop else return error
-        match token_stream.advance() {
-            Some(Token(TokenType::Operator(Operator::Greater), _)) => break,
-            Some(Token(TokenType::Operator(Operator::Comma), _)) => continue,
-            Some(token) => {
-                return Err(ParserError(
-                    "Unexpected token in associated type list".to_string(),
-                    token.1.clone(),
-                ))
-            }
-            None => {
-                return Err(ParserError(
-                    "Expected associated type list close".to_string(),
-                    FileLocation::End { file: file.clone() },
-                ))
-            }
-        }
-    }
-
-    // return associated types
-    return Ok(associated_types);
-}
-
 // Parse a type identifier
 pub fn parse_type(token_stream: &mut TokenStream, file: &PathBuf) -> Result<Type, ParserError> {
     // Get the typename token
